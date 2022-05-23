db8c0247cafc1baefef4a5f32e5cfb9e4bb7fa40
trieutct <96654274+trieutct@users.noreply.github.com>
2022-05-23 22:54:20 +0700

    ádasd

--- js/information.js
+++
@@ -1,6 +0,0 @@
-var btn=document.querySelector(".btn");
-var menu=document.querySelector("#menu");
-btn.addEventListener("click",event=>{
-    btn.classList.toggle("rotateBtn");
-    menu.classList.toggle("showMenu");
-})
